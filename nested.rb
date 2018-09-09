4945b6371a4037c8782ef64d64bef7e0d5121bf6
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-09 15:30:08 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -59,7 +59,9 @@ def dennis_ritchies_language
 end
 
 def adding_matz
+
 programmer_hash[:yukihiro_matsumoto] = {known_for => "Ruby", languages => ["LISP", "C"]}]
+
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
@@ -101,7 +103,7 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = "GUI"
      
      
 end
