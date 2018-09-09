78b6b8d47ed80128b1355af550045096de177ee6
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-09 15:43:21 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:languages]
+  object = programmer_hash[:dennis_ritchie][:languages]
+  object.to_s
 end
 
 def adding_matz
